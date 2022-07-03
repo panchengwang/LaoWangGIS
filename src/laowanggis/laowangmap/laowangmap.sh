dfb4697d78976d03b77945d5447650ed3dd4e427
pc wang <pcwang@pcdeMac-mini.local>
2022-07-04 07:38:24 +0800

    Run on MacOS

--- src/laowanggis/laowangmap/laowangmap.sh
+++ src/laowanggis/laowangmap/laowangmap.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+OS=`uname`
 
 CUR_DIR=$(cd `dirname $0`; pwd)
 export QML2_IMPORT_PATH=${CUR_DIR}/../laowangqml
-${CUR_DIR}/laowangmap
+
+echo $OS
+if test "$OS"="Darwin" ; then
+    ${CUR_DIR}/laowangmap.app/Contents/MacOS/laowangmap
+else
+    ${CUR_DIR}/laowangmap
+fi
+
+
